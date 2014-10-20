464008a0f17a60e779ef26875e7f957b69f3d999
Brian <brian@tykus.ie>
2014-10-21 00:04:11 +0800

    Creating initial home page controller and views

---
+++ app/controllers/PagesController.php
@@ -0,0 +1,15 @@
+<?php
+
+class PagesController extends \BaseController {
+
+	/**
+	 * Display a listing of the resource.
+	 *
+	 * @return Response
+	 */
+	public function home()
+	{
+		return View::make ('pages.home');
+	}
+
+}
