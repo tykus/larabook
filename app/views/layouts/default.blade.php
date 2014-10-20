dcb9e6b6b2767744cb4af86400e60056ccd87f72
Brian <brian@tykus.ie>
2014-10-20 23:53:36 +0800

    Creating the default layout template with Bootstrap 3.2.0 resources

---
+++ app/views/layouts/default.blade.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <title>Larabook</title>
+  <link rel="stylesheet" type="text/css" href="//maxcdn.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap.min.css">
+</head>
+<body>
+
+  <div class="container">
+
+    @yield('content')
+
+  </div>
+  <script src="//maxcdn.bootstrapcdn.com/bootstrap/3.2.0/js/bootstrap.min.js"></script>
+</body>
+</html>
